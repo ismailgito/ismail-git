d0ebe687ea046e933195fd7cf9c918758090a4a3
ismailgito <dadobeb553@gmail.com>
2025-01-25 21:16:22 +0530

    transfer files to guthub

---
+++ python/pass or fail.py
@@ -0,0 +1,10 @@
+Reg=int(input("Register Number:"))
+dd=input("DD")
+mm=input("MM")
+yyyy=input("YYYY")
+DOB=(dd,mm,yyyy)
+print(DOB)
+if (Reg="2113181096157" and DOB="15","02","2003"):
+    print("pass")
+else:
+    print("no data found")
